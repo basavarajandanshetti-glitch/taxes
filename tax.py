955c36ee74b4e5d784ba68036dd1aa7646e35649
basavarajandanshetti-glitch <basavarajandanshetti@gmail.com>
2025-12-04 17:08:11 +0530

    Fix argument count and update print statements

--- tax.py
+++ tax.py
@@ -1,23 +1,20 @@
 import sys
 
-
-if len(sys.argv) == 4:
+if len(sys.argv) == 3:
     script_name = sys.argv[0]
     productprice = int(sys.argv[1])
     tax = int(sys.argv[2])
-    payableprice = int(sys.argv[3])
 
-    print("User Input:")
+    print("User Input Provided:")
 else:
-    
     script_name = sys.argv[0]
     productprice = 1000
-    tax = 20   
-    payableprice = productprice + tax
+    tax = 20
 
     print("Using Default Values:")
-
-
 payableprice = productprice + tax
 
+
+print("Product Price:", productprice)
+print("Tax:", tax)
 print("Payable Price:", payableprice)
